917c8bb94383101c48ce792d4db91933f7254dd8
fabianfa <fabian.faulstich@yahoo.de>
2018-02-08 19:05:20 +0100

    added SingleParticleFunction to wavefunction class

--- wavefunction.hpp
+++ wavefunction.hpp
@@ -1,11 +1,35 @@
 #ifndef WAVEFUNCTION_HPP
 #define WAVEFUNCTION_HPP
 
-class WaveFunction {
+#include "particles.hpp"
+
+struct WaveFunctionParameters {
     double a;
     double alpha;
     double beta;
-    
+    double omega_ho;
+    double omega_z;
+    double hbar;
+}
+
+class SingleParticleFunction{
+    WaveFunctionParameters* parameters;
+
+public:
+    double evaluate(Particles particles, int particle_idx);
+    double evaluate_gradient(Particles particles, int particle_idx);
+    double evaluate_laplace(Particles particles, int particle_idx);
+
+    SingleParticleFunction (& parameters_)
+}
+
+class WaveFunction {
+    WaveFunctionParameters* parameters;
+
+public:
+    double local_energy(Particles particles);
+    double evaluate(Particles particles);
+    double evaluate_PDF(Particles particle);
 
 };
 
