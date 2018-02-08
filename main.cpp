4dc8b9ab6430e5d6eb507512c6cefbab8a9c8af7
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2018-02-08 18:31:59 +0100

    added mass to the particles

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    Particles our_first_particles(5, 3);
+    Particles our_first_particles(5, 3, 1);
 
     vector<double> r = {1, 2, 3, 4, 5, 6};
     //Particles our_second_particles(2, 3, r);
