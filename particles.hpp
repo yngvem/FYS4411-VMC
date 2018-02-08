d1f7b20b72ad1bdcc8b9095baaee72c30fb62009
fabianfa <fabian.faulstich@yahoo.de>
2018-02-08 15:28:24 +0100

    fix typos

--- particles.hpp
+++ particles.hpp
@@ -7,17 +7,17 @@
 using namespace std;
 
 class Particles {
-    int n_particles;
-    int n_dimensions;
+    int num_particles;
+    int num_dimensions;
     vector<double> R;
 
-    vector<double> initial_R(int num_dimensions, int num_particles);
-    vector<double> initial_r(int num_dimensions);
+    vector<double> initial_R(int num_dimensions_, int num_particles_);
+    vector<double> initial_r(int num_dimensions_);
 
 public:
     vector<double> get_particle (int particle_number);
-    Particles (int num_particles, int num_dimensions);
-    Particles (int num_particles, int num_dimensions, int R);
+    Particles (int num_particles_, int num_dimensions_);
+    Particles (int num_particles_, int num_dimensions_, vector<double> R_);
 };
 
 
