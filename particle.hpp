8f2cf4ce17967b9b8585435d0c987f6f2bf1b28d
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2018-02-09 08:50:23 +0100

    Created single particle header file

---
+++ particle.hpp
@@ -0,0 +1,36 @@
+#ifndef PARTICLES
+#define PARTICLES
+
+#include <vector>
+
+using namespace std;
+
+class Particle {
+    int num_dimensions;
+    vector<double> r;
+    double m;
+
+    void initiate_r();
+
+public:
+    Particle(int num_dimensions, double mass);
+    Particle(int num_dimensions);
+
+    // Obtaining information about the position.
+    const vector<double>& position() const;
+    const double norm() const;
+    const double& operator[](int i);
+
+    // Arithmetic operations between multiple particles.
+    Particle operator+(const Particle& particle);
+    Particle operator-(const Particle& particle);
+    Particle operator*(const Particle& particle);
+    
+    // Arithmetic operations between a particle and a number.
+    Particle operator+(double a);
+    Particle operator*(double a);
+    Particle operator-(double a);
+    Particle operator/(double a);
+};
+
+#endif
