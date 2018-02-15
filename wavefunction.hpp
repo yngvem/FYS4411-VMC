89c30d475da4e8606e41a74c77c8526e1773ce4e
fabianfa <fabian.faulstich@yahoo.de>
2018-02-15 16:49:15 +0100

    added wavefunction cpp

--- wavefunction.hpp
+++ wavefunction.hpp
@@ -1,6 +1,7 @@
 #ifndef WAVEFUNCTION_HPP
 #define WAVEFUNCTION_HPP
 
+#include <cmath>
 #include "particles.hpp"
 
 struct WaveFunctionParameters {
@@ -9,28 +10,38 @@ struct WaveFunctionParameters {
     double beta;
     double omega_ho;
     double omega_z;
-    double hbar;
-}
+    double hbar; 
+};
 
-class SingleParticleFunction{
+class SingleParticleFunction {
     WaveFunctionParameters* parameters;
 
 public:
     double evaluate(Particles particles, int particle_idx);
-    double evaluate_gradient(Particles particles, int particle_idx);
+    vector<double> evaluate_gradient(Particles particles, int particle_idx);
     double evaluate_laplace(Particles particles, int particle_idx);
 
-    SingleParticleFunction (& parameters_)
-}
+    SingleParticleFunction ();
+};
 
 class WaveFunction {
     WaveFunctionParameters* parameters;
 
+    double diff_function_u(Particles particles, int particle_idx_1,
+                           int particle_idx_2);
+    double first_deriv_u(Particles particles, int particle_idx_1,
+                           int particle_idx_2);
+    double second_deriv_u(Particles particles, int particle_idx_1,
+                           int particle_idx_2);
+
 public:
+
     double local_energy(Particles particles);
     double evaluate(Particles particles);
     double evaluate_PDF(Particles particle);
 
+
+    WaveFunction ();
 };
 
 
